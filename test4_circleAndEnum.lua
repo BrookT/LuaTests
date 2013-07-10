c7e0844960f1c35140756e85468d19540595a4ea
Brook Tang <787305552@qq.com>
2013-07-10 14:05:03 +0800

    Create test4_circleAndEnum.lua

---
+++ test4_circleAndEnum.lua
@@ -0,0 +1,20 @@
+-- 2013-7-10 14:03 test of enum and circle
+
+days={
+"Sunday",
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday"
+};
+
+for i,v in ipairs(days) do
+  print(v);
+end
+
+function printDay(x)
+	print(days[x]);
+end
+--print(days[1]);
