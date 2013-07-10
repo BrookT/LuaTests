30d6e5185d159318c3dfd2d453d1484e93fd8b91
Brook Tang <787305552@qq.com>
2013-07-10 13:49:48 +0800

    Create Test3_stringOperation.lua

---
+++ Test3_stringOperation.lua
@@ -0,0 +1,14 @@
+-- test 3 2013-7-10
+--[[print(type("hello world"));
+print(type(10.4*3));
+print(type(print));
+print(type(type));
+print(type(true));
+print(type(nil));
+print(type(type(x)));--]]
+
+print(string.upper("this is a upper test"));
+print(string.sub("asdsdadsadsasd",5,10));
+print(string.reverse("this is a reverse test"));
+print(string.rep("base",3));
+
